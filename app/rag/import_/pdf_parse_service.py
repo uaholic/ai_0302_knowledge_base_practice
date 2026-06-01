26a825b51dfc4eae4493ad84d31340d8559bf3a2
GuanYanqi <admin@guanyanqi.com>
2026-06-01 11:37:23 +0800

    实现下载文件并解压

--- app/rag/import_/pdf_parse_service.py
+++ app/rag/import_/pdf_parse_service.py
@@ -1,3 +1,4 @@
+import shutil
 import time
 
 import requests
@@ -7,8 +8,9 @@ from pathlib import Path
 
 from app.rag.import_.config import MINERU_MODEL_VERSION, MINERU_DOWNLOAD_TIMEOUT_SECONDS, MINERU_POLL_TIMEOUT_SECONDS, \
     MINERU_POLL_INTERVAL_SECONDS
-from app.shared.runtime.logger import logger , PROJECT_ROOT
+from app.shared.runtime.logger import logger, PROJECT_ROOT, step_log
 from app.infra.config.providers import infra_config
+
 
 #   validate_pdf_paths(state: ImportState) -> tuple[Path pdf_path_obj, Path local_dir_path_obj]
 #         1. 读取 `pdf_path` 和 `local_dir`
@@ -20,6 +22,7 @@ from app.infra.config.providers import infra_config
 #         7. 返回 `pdf_path_obj` 与 `local_dir_obj`
 
 # 1. pdf dir路径校验和完善
+@step_log("validate_pdf_paths")
 def validate_pdf_paths(state: ImportGraphState) -> tuple[Path, Path]:
     """
     路径参数校验
@@ -38,20 +41,21 @@ def validate_pdf_paths(state: ImportGraphState) -> tuple[Path, Path]:
         logger.warning(f"进行pdf转化md,但是发现local_dir为空,我们给与默认值 项目/output")
         local_dir = PROJECT_ROOT / "output"  # Path
     # 4. 转换为 `Path` 对象
-    pdf_path_obj = Path(pdf_path) # 后续对文件存在性校验! 有他更方便
-    local_dir_obj = Path(local_dir) # 这块不报错
+    pdf_path_obj = Path(pdf_path)  # 后续对文件存在性校验! 有他更方便
+    local_dir_obj = Path(local_dir)  # 这块不报错
     # 5. 校验 PDF 文件是否真实存在
     if not pdf_path_obj.exists():
         logger.error(f"进行pdf转化md,pdf_path值:{pdf_path},但是不存在对应的文件,业务无法继续!!")
         raise FileNotFoundError(f"进行pdf转化md,pdf_path值:{pdf_path},但是不存在对应的文件,业务无法继续!!")
     # 6. 若输出目录不存在，则自动创建
-    if not local_dir_obj.exists() :
+    if not local_dir_obj.exists():
         logger.warning(f"进行pdf转化md,local_dir值:{local_dir_obj},但是没有对应文件夹! 我们自行创建!!")
         # parents 自动创建多层文件夹  例如: x/x/x/x  -> True会自动创建 x  x   x  x
         # exist_ok = True 当存在的时候不会创建也不会报错!
-        local_dir_obj.mkdir(parents=True,exist_ok=True)
+        local_dir_obj.mkdir(parents=True, exist_ok=True)
     # 7. 返回 `pdf_path_obj` 与 `local_dir_obj`
-    return pdf_path_obj,local_dir_obj
+    return pdf_path_obj, local_dir_obj
+
 
 # upload_pdf_and_poll(pdf_path_obj: Path) -> str
 #        1. 校验 MinerU 配置是否完整
@@ -60,8 +64,8 @@ def validate_pdf_paths(state: ImportGraphState) -> tuple[Path, Path]:
 #        4. 根据 `batch_id` 轮询任务状态
 #        5. 若任务成功，返回 `full_zip_url`
 #        6. 若任务失败或超时，抛出异常
-
-def upload_pdf_and_poll(pdf_path_obj:Path) -> str:
+@step_log("upload_pdf_and_poll")
+def upload_pdf_and_poll(pdf_path_obj: Path) -> str:
     """
        minerU交互
     :param pdf_path_obj:  上传文件的path对象
@@ -85,22 +89,23 @@ def upload_pdf_and_poll(pdf_path_obj:Path) -> str:
         "model_version": MINERU_MODEL_VERSION
     }
     try:
-        response =  requests.post(url,headers=header,json= data, timeout=MINERU_DOWNLOAD_TIMEOUT_SECONDS)
+        response = requests.post(url, headers=header, json=data, timeout=MINERU_DOWNLOAD_TIMEOUT_SECONDS)
 
         # 状态码是否正常200  (网络状态 服务器状态)
         if response.status_code != 200:
             logger.error(f"服务器发生异常!无法进行业务!响应状态码为:{response.status_code}")
             raise RuntimeError(f"服务器发生异常!无法进行业务!响应状态码为:{response.status_code}")
         # 判断业务是否正常0  (业务状态)
-        response_dict  = response.json()
+        response_dict = response.json()
         code = response_dict.get("code")
         if code != 0:
             logger.error(f"业务处理发生异常! 业务状态码为:{code},异常信息:{response_dict.get('msg')}")
             raise RuntimeError(f"业务处理发生异常! 业务状态码为:{code},异常信息:{response_dict.get('msg')}")
 
-        batch_id = response_dict.get("data",{}).get("batch_id")
-        upload_file_url = response_dict.get("data",{}).get("file_urls")[0]
-        logger.info(f"调用 `/file-urls/batch` 申请上传地址与 `batch_id`, batch_id:{batch_id},上传地址:{upload_file_url}")
+        batch_id = response_dict.get("data", {}).get("batch_id")
+        upload_file_url = response_dict.get("data", {}).get("file_urls")[0]
+        logger.info(
+            f"调用 `/file-urls/batch` 申请上传地址与 `batch_id`, batch_id:{batch_id},上传地址:{upload_file_url}")
     except Exception as e:
         logger.exception(f"向minerU申请上传文件地址发生异常! url参数: {url},key参数:{token}")
         raise e
@@ -112,11 +117,13 @@ def upload_pdf_and_poll(pdf_path_obj:Path) -> str:
             # session使用和requests是一样的
             # 作用1: 可以复用请求 requests.Session() session.get post    session.close() [根本不服用]
             # 作用2: 有些特殊的设置 trust_env = False 我谁也不信!!! 向预签名地址传递数据避免干扰成功率更高!!
-            put_response = session.put(upload_file_url,data=pdf_path_obj.read_bytes())
+            put_response = session.put(upload_file_url, data=pdf_path_obj.read_bytes())
             # status_code |  code
             if put_response.status_code != 200:
-                logger.error(f"向地址:{upload_file_url}上传文件发生异常,状态码:{put_response.status_code},业务无法继续!!")
-                raise RuntimeError(f"向地址:{upload_file_url}上传文件发生异常,状态码:{put_response.status_code},业务无法继续!!")
+                logger.error(
+                    f"向地址:{upload_file_url}上传文件发生异常,状态码:{put_response.status_code},业务无法继续!!")
+                raise RuntimeError(
+                    f"向地址:{upload_file_url}上传文件发生异常,状态码:{put_response.status_code},业务无法继续!!")
     except Exception as e:
         logger.exception(f"向minerU文件服务器,上传文件发生异常{str(e)}! 业务无继续!!")
         raise e
@@ -125,7 +132,7 @@ def upload_pdf_and_poll(pdf_path_obj:Path) -> str:
     # 前置准备工作
     get_zip_url = f"{infra_config.mineru.base_url}/extract-results/batch/{batch_id}"
     timeout = MINERU_POLL_TIMEOUT_SECONDS  # 600
-    interval_time = MINERU_POLL_INTERVAL_SECONDS # 3
+    interval_time = MINERU_POLL_INTERVAL_SECONDS  # 3
     start_time = time.time()
 
     while True:
@@ -136,7 +143,7 @@ def upload_pdf_and_poll(pdf_path_obj:Path) -> str:
             raise TimeoutError(f"轮询获取:{batch_id}结果超时! 用时:{time.time() - start_time}")
         # 2. 发起网络请求(报错,再给一次机会)
         try:
-            get_response = requests.get(get_zip_url,headers=header)
+            get_response = requests.get(get_zip_url, headers=header)
         except Exception as e:
             logger.warning(f"获取下载的zipurl地址, 网络请求失败!等待后继续尝试!")
             time.sleep(interval_time)
@@ -147,11 +154,14 @@ def upload_pdf_and_poll(pdf_path_obj:Path) -> str:
             # 一定是错误了,看这个错误是否给机会! 5xx
             if 500 <= get_response.status_code < 600:
                 # 给机会
-                logger.warning(f"获取下载的zipurl地址,minerU对应服务器发生异常! 状态码:{get_response.status_code},等待后再次尝试!!")
-                time.sleep(interval_time+2)
+                logger.warning(
+                    f"获取下载的zipurl地址,minerU对应服务器发生异常! 状态码:{get_response.status_code},等待后再次尝试!!")
+                time.sleep(interval_time + 2)
                 continue
-            logger.error(f"获取下载的zipurl地址,minerU对应服务器发生异常! 状态码:{get_response.status_code},业务无法继续了!")
-            raise RuntimeError(f"获取下载的zipurl地址,minerU对应服务器发生异常! 状态码:{get_response.status_code},业务无法继续了!")
+            logger.error(
+                f"获取下载的zipurl地址,minerU对应服务器发生异常! 状态码:{get_response.status_code},业务无法继续了!")
+            raise RuntimeError(
+                f"获取下载的zipurl地址,minerU对应服务器发生异常! 状态码:{get_response.status_code},业务无法继续了!")
 
         # 4. 判断code
         get_response_dict = get_response.json()
@@ -159,12 +169,13 @@ def upload_pdf_and_poll(pdf_path_obj:Path) -> str:
             logger.error(
                 f"获取下载的zipurl地址,minerU对应服务器发生异常! 业务码:{get_response_dict.get('code')} ,错误信息:"
                 f"{get_response_dict.get('msg')},业务无法继续了!")
-            raise RuntimeError( f"获取下载的zipurl地址,minerU对应服务器发生异常! 业务码:{get_response_dict.get('code')} ,错误信息:"
+            raise RuntimeError(
+                f"获取下载的zipurl地址,minerU对应服务器发生异常! 业务码:{get_response_dict.get('code')} ,错误信息:"
                 f"{get_response_dict.get('msg')},业务无法继续了!")
         # 5. 获取结果信息(是否解析完毕)  正在解析 循环  解析完毕 获取结果 return 解析失败 抛出异常
         # 获取结果的dict
-        result_dict = get_response_dict.get("data",{}).get("extract_result",[])[0]
-        result_state = result_dict.get("state","failed")
+        result_dict = get_response_dict.get("data", {}).get("extract_result", [])[0]
+        result_state = result_dict.get("state", "failed")
 
         if result_state == "done":
             full_zip_url = result_dict.get("full_zip_url")
@@ -186,26 +197,51 @@ def upload_pdf_and_poll(pdf_path_obj:Path) -> str:
         time.sleep(interval_time)
 
 
+@step_log("download_zip_file")
+def download_zip_file(zip_url: str, local_dir: Path, stem: str) -> Path:
+    response = requests.get(zip_url, timeout=MINERU_DOWNLOAD_TIMEOUT_SECONDS)
+    if response.status_code != 200:
+        logger.error(f"下载文件发生异常,状态码:{response.status_code},业务无法继续!!")
+        raise RuntimeError(f"下载文件发生异常,状态码:{response.status_code},业务无法继续!!")
+
+    zip_file_path_obj = local_dir / f"{stem}.zip"
+    zip_file_path_obj.write_bytes(response.content)
+
+    unzip_dir_obj = local_dir / stem
+    if unzip_dir_obj.is_dir():
+        logger.warning(f"{unzip_dir_obj}已存在,删除!!")
+        shutil.rmtree(unzip_dir_obj)
+
+    unzip_dir_obj.mkdir(parents=True, exist_ok=True)
+    shutil.unpack_archive(zip_file_path_obj, unzip_dir_obj)
+
+    unzip_md_list = list(unzip_dir_obj.rglob("*.md"))
+
+    if not unzip_md_list or len(unzip_md_list) == 0:
+        logger.error(f"{zip_url}下载后解压的{unzip_dir_obj}解析结果没有md文件!!")
+        raise RuntimeError(f"{zip_url}下载后解压的{unzip_dir_obj}解析结果没有md文件!!")
+
+    for md_path in unzip_md_list:
+        if md_path.name == f"{stem}.md":
+            logger.info(f"{zip_url}下载后解压的{unzip_dir_obj}解析结果有{stem}.md同名文件,直接返回!!")
+            return md_path
+
+    md_path_obj = None
+    for md_path in unzip_md_list:
+        if md_path.name.lower() == "full.md":
+            logger.info(f"{zip_url}下载后解压的{unzip_dir_obj}解析结果有full.md文件,使用此文件执行业务!!")
+            md_path_obj = md_path
+
+    if not md_path_obj:
+        md_path_obj= unzip_md_list[0]
+        logger.warning(f"{zip_url}下载后解压的{unzip_dir_obj}解析结果没有{stem}.md文件,默认使用第一个md文件：{md_path_obj.name}代替!!")
+
+    logger.info(f"将文件{str(md_path_obj)}重命名为{md_path_obj.with_name(f"{stem}.md")}")
+
+    return md_path_obj.rename(md_path_obj.with_name(f"{stem}.md"))
 
 
-    token = "官网申请的api token"
-    batch_id = "上一步批量提交返回的 batch_id"
-    url = f"https://mineru.net/api/v4/extract-results/batch/{batch_id}"
-    header = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {token}"
-    }
-
-    res = requests.get(url, headers=header)
-    print(res.status_code)
-    print(res.json())
-    print(res.json()["data"])
-
-
-    #        5. 若任务成功，返回 `full_zip_url`
-    #        6. 若任务失败或超时，抛出异常
-
-
+@step_log("parse_pdf_to_markdown")
 def parse_pdf_to_markdown(state: ImportGraphState) -> ImportGraphState:
     """
     PDF 解析服务：
@@ -215,10 +251,15 @@ def parse_pdf_to_markdown(state: ImportGraphState) -> ImportGraphState:
     4. 回写 md_path / md_content / local_dir
     """
     # 1. pdf dir路径校验和完善
-    pdf_path_obj , local_dir_obj = validate_pdf_paths(state)
+    pdf_path_obj, local_dir_obj = validate_pdf_paths(state)
 
     # 2. pdf上传和zip url地址获取
     zip_url = upload_pdf_and_poll(pdf_path_obj)
+
+    md_path_obj = download_zip_file(zip_url, local_dir_obj, pdf_path_obj.stem)
+
+    state["md_path"]=str(md_path_obj)
+    state["md_content"] = md_path_obj.read_text(encoding="utf-8")
 
     print(zip_url)
     return state
